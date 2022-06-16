a5b0359167eaf8c937734fe1421e7f200cd4dc4a
Xacor <ryabov.kostantin18@gmail.com>
2022-06-16 18:03:13 +0300

    Added Books, Images, Reviews, Genres, books_genres

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
+import os
+from flask import url_for
 import sqlalchemy as sa
 from app import db, app
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from users_policy import UsersPolicy
+from sqlalchemy.dialects.mysql import YEAR
 
 
 class User(db.Model, UserMixin):
@@ -51,7 +54,69 @@ class Role(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    desc = db.Column(db.Text)
+    desc = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    desc = db.Column(db.Text, nullable=False)
+    year = db.Column(YEAR, nullable=False)
+    publisher = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    volume = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Book %r>' % self.name
+
+class Image(db.Model):
+    _tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(100), nullable=False)
+    mime_type = db.Column(db.String(100), nullable=False)
+    md5_hash = db.Column(db.String(100), nullable=False, unique=True)
+    
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.file_name
+
+    @property
+    def storage_filename(self):
+        _, ext = os.path.splitext(self.file_name)
+        return self.id + ext
+
+    @property
+    def url(self):
+        return url_for('image', image_id=self.id)
+
+class Review(db.Model):
+    __tablename__='reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=sa.sql.func.now())
+    
+    user = db.relationship('User', uselist=False)
+    book = db.relationship('Book', uselist=False)
+
+class Genre(db.Model):
+    __tablename__='genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+
+books_genres = db.Table('books_genres',
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), nullable=False),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genres.id'), nullable=False)
+)
